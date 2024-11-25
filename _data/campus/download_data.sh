89622b5e81166e87fc71b75b627670a62cbb26f6
sebastian <sebastian.cavada.lab@gmail.com>
2024-11-25 09:20:41 +0400

    small fixes

--- _data/campus/download_data.sh
+++ _data/campus/download_data.sh
@@ -1,20 +1,20 @@
-# mkdir -p campus/section_01/00/video
-# mkdir -p campus/section_01/01/video
+mkdir -p campus/section_01/00/video
+mkdir -p campus/section_01/01/video
 
 # wget https://mbzuaiac-my.sharepoint.com/:v:/g/personal/sebastian_cavada_mbzuai_ac_ae/EX8AQwFx6gFAuYbIxdSbLvMBPc3hz3lnFvtUZNCd7NNywA?nav=eyJyZWZlcnJhbEluZm8iOnsicmVmZXJyYWxBcHAiOiJPbmVEcml2ZUZvckJ1c2luZXNzIiwicmVmZXJyYWxBcHBQbGF0Zm9ybSI6IldlYiIsInJlZmVycmFsTW9kZSI6InZpZXciLCJyZWZlcnJhbFZpZXciOiJNeUZpbGVzTGlua0NvcHkifX0&e=uXyffF&download=1 -o ./campus/section_01/00/video/GX010041.MP4
 # wget https://mbzuaiac-my.sharepoint.com/:v:/g/personal/sebastian_cavada_mbzuai_ac_ae/EX8AQwFx6gFAuYbIxdSbLvMBPc3hz3lnFvtUZNCd7NNywA?nav=eyJyZWZlcnJhbEluZm8iOnsicmVmZXJyYWxBcHAiOiJPbmVEcml2ZUZvckJ1c2luZXNzIiwicmVmZXJyYWxBcHBQbGF0Zm9ybSI6IldlYiIsInJlZmVycmFsTW9kZSI6InZpZXciLCJyZWZlcnJhbFZpZXciOiJNeUZpbGVzTGlua0NvcHkifX0&e=uXyffF&download=1 -o ./campus/section_01/01/video/GX010026.MP4
 
-# mkdir -p _data/campus_big_0.1fps
-# mkdir -p _data/campus_big_1.0fps
-# mkdir -p _data/campus_big_5.0fps
-# mkdir -p _data/campus_big_sparse
-# mkdir -p _data/campus_test
+mkdir -p _data/campus_big_0.1fps
+mkdir -p _data/campus_big_1.0fps
+mkdir -p _data/campus_big_5.0fps
+mkdir -p _data/campus_big_sparse
+mkdir -p _data/campus_test
 
 
 # campus 00      
-# wget 'https://mbzuaiac-my.sharepoint.com/:v:/g/personal/sebastian_cavada_mbzuai_ac_ae/EVS6AaAMhpdDjSCfQ1GQkbEBBVRk4FSK-D1Xjwt-n9aicA?nav=eyJyZWZlcnJhbEluZm8iOnsicmVmZXJyYWxBcHAiOiJPbmVEcml2ZUZvckJ1c2luZXNzIiwicmVmZXJyYWxBcHBQbGF0Zm9ybSI6IldlYiIsInJlZmVycmFsTW9kZSI6InZpZXciLCJyZWZlcnJhbFZpZXciOiJNeUZpbGVzTGlua0NvcHkifX0&e=bpWowW&download=1' -O ./_data/campus/GX010049.MP4
-# wget 'https://mbzuaiac-my.sharepoint.com/:v:/g/personal/sebastian_cavada_mbzuai_ac_ae/EacPKEGTlBJNmboa2p60YHMBsaddStZezFWpGDPZOxX4LA?nav=eyJyZWZlcnJhbEluZm8iOnsicmVmZXJyYWxBcHAiOiJPbmVEcml2ZUZvckJ1c2luZXNzIiwicmVmZXJyYWxBcHBQbGF0Zm9ybSI6IldlYiIsInJlZmVycmFsTW9kZSI6InZpZXciLCJyZWZlcnJhbFZpZXciOiJNeUZpbGVzTGlua0NvcHkifX0&e=ImSGbI&download=1' -O ./GX020049.MP4
+wget 'https://mbzuaiac-my.sharepoint.com/:v:/g/personal/sebastian_cavada_mbzuai_ac_ae/EVS6AaAMhpdDjSCfQ1GQkbEBBVRk4FSK-D1Xjwt-n9aicA?nav=eyJyZWZlcnJhbEluZm8iOnsicmVmZXJyYWxBcHAiOiJPbmVEcml2ZUZvckJ1c2luZXNzIiwicmVmZXJyYWxBcHBQbGF0Zm9ybSI6IldlYiIsInJlZmVycmFsTW9kZSI6InZpZXciLCJyZWZlcnJhbFZpZXciOiJNeUZpbGVzTGlua0NvcHkifX0&e=bpWowW&download=1' -O ./GX010049.MP4
+#wget 'https://mbzuaiac-my.sharepoint.com/:v:/g/personal/sebastian_cavada_mbzuai_ac_ae/EacPKEGTlBJNmboa2p60YHMBsaddStZezFWpGDPZOxX4LA?nav=eyJyZWZlcnJhbEluZm8iOnsicmVmZXJyYWxBcHAiOiJPbmVEcml2ZUZvckJ1c2luZXNzIiwicmVmZXJyYWxBcHBQbGF0Zm9ybSI6IldlYiIsInJlZmVycmFsTW9kZSI6InZpZXciLCJyZWZlcnJhbFZpZXciOiJNeUZpbGVzTGlua0NvcHkifX0&e=ImSGbI&download=1' -O ./GX020049.MP4
 
 # campus 01
-wget 'https://mbzuaiac-my.sharepoint.com/:v:/g/personal/sebastian_cavada_mbzuai_ac_ae/EfKk9e8X_D1ErlhPIPgRG28BOLGR5HBNBoZVkGu1MW2gpw?nav=eyJyZWZlcnJhbEluZm8iOnsicmVmZXJyYWxBcHAiOiJPbmVEcml2ZUZvckJ1c2luZXNzIiwicmVmZXJyYWxBcHBQbGF0Zm9ybSI6IldlYiIsInJlZmVycmFsTW9kZSI6InZpZXciLCJyZWZlcnJhbFZpZXciOiJNeUZpbGVzTGlua0NvcHkifX0&e=r77zJ2&download=1' -O ./GX010044.MP4
-# wget 'https://mbzuaiac-my.sharepoint.com/:v:/g/personal/sebastian_cavada_mbzuai_ac_ae/EeOe87ehripBiGNQD_IcnVABFTblBAM8mIp_Bw6AfaRo_w?nav=eyJyZWZlcnJhbEluZm8iOnsicmVmZXJyYWxBcHAiOiJPbmVEcml2ZUZvckJ1c2luZXNzIiwicmVmZXJyYWxBcHBQbGF0Zm9ybSI6IldlYiIsInJlZmVycmFsTW9kZSI6InZpZXciLCJyZWZlcnJhbFZpZXciOiJNeUZpbGVzTGlua0NvcHkifX0&e=84Ao91&download=1' -O ./GX020044.MP4
+#wget 'https://mbzuaiac-my.sharepoint.com/:v:/g/personal/sebastian_cavada_mbzuai_ac_ae/EfKk9e8X_D1ErlhPIPgRG28BOLGR5HBNBoZVkGu1MW2gpw?nav=eyJyZWZlcnJhbEluZm8iOnsicmVmZXJyYWxBcHAiOiJPbmVEcml2ZUZvckJ1c2luZXNzIiwicmVmZXJyYWxBcHBQbGF0Zm9ybSI6IldlYiIsInJlZmVycmFsTW9kZSI6InZpZXciLCJyZWZlcnJhbFZpZXciOiJNeUZpbGVzTGlua0NvcHkifX0&e=r77zJ2&download=1' -O ./GX010044.MP4
+#wget 'https://mbzuaiac-my.sharepoint.com/:v:/g/personal/sebastian_cavada_mbzuai_ac_ae/EeOe87ehripBiGNQD_IcnVABFTblBAM8mIp_Bw6AfaRo_w?nav=eyJyZWZlcnJhbEluZm8iOnsicmVmZXJyYWxBcHAiOiJPbmVEcml2ZUZvckJ1c2luZXNzIiwicmVmZXJyYWxBcHBQbGF0Zm9ybSI6IldlYiIsInJlZmVycmFsTW9kZSI6InZpZXciLCJyZWZlcnJhbFZpZXciOiJNeUZpbGVzTGlua0NvcHkifX0&e=84Ao91&download=1' -O ./GX020044.MP4
--- _data/campus/prepare_data.sh
+++ _data/campus/prepare_data.sh
@@ -1,35 +1,39 @@
 echo "Preparing for 0.1fps"
 
-cd campus_big_0.1fps
+#cd campus_big_0.1fps
+#
+#mkdir -p 00
+#cd 00
+#mkdir ./images
+#ffmpeg -hwaccel auto -i ../../GX010049.MP4 -vf "fps=0.1,scale=iw:ih" -q:v 2 ./images/%06d.jpg
+#cd ..
+#
+#mkdir -p 01
+#cd 01
+#mkdir ./images
+#ffmpeg -hwaccel auto -i ../../GX010044.MP4 -vf "fps=0.1,scale=iw:ih" -q:v 2 ./images/%06d.jpg
+#cd ../../
+#cp ../run.sh ./run.sh
 
-# mkdir -p 00
-# cd 00
-# mkdir ./images
-# ffmpeg -hwaccel auto -i ../../GX010049.MP4 -vf "fps=0.1,scale=iw:ih" -q:v 2 ./images/%06d.jpg
-# cd ..
 
-# mkdir -p 01
-# cd 01
-# mkdir ./images
-# ffmpeg -hwaccel auto -i ../../GX010044.MP4 -vf "fps=0.1,scale=iw:ih" -q:v 2 ./images/%06d.jpg
+#echo "Preparing for 1 fps"
+#
+cd campus_big_1.0fps/
+
+mkdir -p 00
+cd 00
+mkdir ./images
+ffmpeg -hwaccel auto -i ../../GX010049.MP4 -vf "fps=1,scale=iw:ih" -q:v 2 ./images/%06d.jpg
+cd ..
+
+mkdir -p 01
+cd 01
+mkdir ./images
+ffmpeg -hwaccel auto -i ../../GX010044.MP4 -vf "fps=1,scale=iw:ih" -q:v 2 ./images/%06d.jpg
+
 cd ../../
 cp ../run.sh ./run.sh
 
-
-# echo "Preparing for 1 fps"
-
-# cd /campus/campus_big_1.0fps/
-
-# cd 00
-# mkdir ./images
-# ffmpeg -hwaccel auto -i ../GX010049.MP4 -vf "fps=1,scale=iw:ih" -q:v 2 ./images/%06d.jpg
-# cd ..
-
-# cd 01
-# mkdir ./images
-# ffmpeg -hwaccel auto -i ../GX010044.MP4 -vf "fps=1,scale=iw:ih" -q:v 2 ./images/%06d.jpg
-# cp ../../../run.sh ./run.sh
-# cd ../../
 
 
 # echo "Preparing for 5 fps"
